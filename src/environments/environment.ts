ecfaa2f41f8123ebe789881287ef5365fb852f6b
Samuel <upwork@samybaxy.net>
2019-09-16 21:09:52 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: false,
-  appID: '0d630fbc7e1b3bcce95e7727dca457a0',
+  appID: 'XXX',
   firebase: {
-    apiKey: "AIzaSyDZvclPbwIY4zj76qyugV_RQ-1Mod56f2w",
-    authDomain: "minimus-samybaxy-1.firebaseapp.com",
-    databaseURL: "https://minimus-samybaxy-1.firebaseio.com",
-    projectId: "minimus-samybaxy-1",
+    apiKey: "XXX",
+    authDomain: "XXX",
+    databaseURL: "XXX",
+    projectId: "XXX",
     storageBucket: "",
-    messagingSenderId: "393132863004",
-    appId: "1:393132863004:web:fe2b6bc3cdf9597c"
+    messagingSenderId: "XXX",
+    appId: "1:XXX"
   }
 };
 
